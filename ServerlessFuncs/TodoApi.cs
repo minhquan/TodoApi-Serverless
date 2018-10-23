612ab42a6c3c8a0e79d6be27eb890be13e31d5c2
Quan Nguyen <minhquanit@gmail.com>
2018-10-23 15:40:28 -0500

    In-memory tasks

--- ServerlessFuncs/TodoApi.cs
+++ ServerlessFuncs/TodoApi.cs
@@ -7,27 +7,86 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace ServerlessFuncs
 {
     public static class TodoApi
     {
-        [FunctionName("TodoApi")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        static List<Todo> items = new List<Todo>();
 
-            string name = req.Query["name"];
+        /// <summary>
+        /// POST /api/todo
+        /// </summary>
+        [FunctionName("CreateTodo")]
+        public static async Task<IActionResult> CreateTodo([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Creating a new todo list item");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
+
+            var todo = new Todo() { TaskDescription = input.TaskDescription };
+            items.Add(todo);
+            return new OkObjectResult(todo);
+        }
+
+        /// <summary>
+        /// GET /api/todo/
+        /// </summary>
+        [FunctionName("GetTodos")]
+        public static IActionResult GetTodos([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Getting todo list items");
+            return new OkObjectResult(items);
+        }
+
+        /// <summary>
+        /// GET /api/todo/{id}
+        /// </summary>
+        [FunctionName("GetTodoById")]
+        public static IActionResult GetTodoById([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+                return new NotFoundResult();
+
+            return new OkObjectResult(todo);
+        }
+
+        /// <summary>
+        /// PUT /api/todo/{id}
+        /// </summary>
+        [FunctionName("UpdateTodo")]
+        public static async Task<IActionResult> UpdateTodo([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+                return new NotFoundResult();
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var updated = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            todo.IsCompleted = updated.IsCompleted;
+            if (!string.IsNullOrEmpty(updated.TaskDescription))
+                todo.TaskDescription = updated.TaskDescription;
+
+            return new OkObjectResult(todo);
+        }
+
+        /// <summary>
+        /// DELETE /api/todo/{id}
+        /// </summary>
+        [FunctionName("DeleteTodo")]
+        public static IActionResult DeleteTodo([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+                return new NotFoundResult();
+
+            items.Remove(todo);
+            return new OkResult();
         }
     }
 }
